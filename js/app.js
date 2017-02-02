4f2aefdf2c8a8aacd88d95d7da292cb50516381d
Aisha <aishaprograms@gmail.com>
2017-02-01 23:43:49 -0600

    set up js file to take input

--- js/app.js
+++ js/app.js
@@ -2,6 +2,21 @@ $(document).on('click', '#search-button', function() {
     $('#search-form').toggle('slow');
 });
 
+$(document).on('click', '#submit-button', function(event) {
+    event.preventDefault();
+    var searchTerm = $('#search-term').val().trim();
+    if (searchTerm === '') {
+        alert('Please enter a valid input');
+    } else {
+        console.log(searchTerm);
+        $('#search-form')[0].reset();
+        var resultDiv = $('div');
+        resultDiv.addClass('result-div');
+        resultDiv.text(searchTerm);
+        $('#results-div').append(resultDiv);
+    }
+});
+
 // var queryUrl = 'https://en.wikipedia.org/w/api.php?' + 'format=json' + '&action=query' + '&generator=search' +
 //     '&prop=pageimages|extracts' + '&titles=cats';
 
